c38fdf4c3d4d8eb6983de5bb6f4c8d207eb90627
tomohito0358 <tomohito0358@gmail.com>
2020-12-30 18:14:23 +0900

    Basic認証を導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   end
 
   def checked
-    binding.pry
+    # binding.pry
     post = Post.find(params[:id])
     if post.checked
       post.update(checked: false)
